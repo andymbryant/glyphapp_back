5fad76bf791c1d1f7a6f30c47551b0c8fd427d5e
Andy Bryant <a.mcdermid.bryant@gmail.com>
2018-03-26 20:42:14 -0700

    V1 Complete

--- app.js
+++ app.js
@@ -83,105 +83,105 @@ app.post('/save', function (req, res) {
             })
         }
 
-        // else if (req.body.data[i][0] === 'DOTA_COMBATLOG_HEAL') {
-        //     Heal.create({
-        //         timestamp: req.body.data[i][1],
-        //         source_name: req.body.data[i][2],
-        //         target_name: req.body.data[i][3],
-        //         inflictor_name: req.body.data[i][4],
-        //         heal_value: req.body.data[i][5],
-        //         health_remaining: req.body.data[i][6]
-        //     })
-        // }
-        //
-        // else if (req.body.data[i][0] === 'DOTA_COMBATLOG_MODIFIER_ADD') {
-        //     ModifierAdd.create({
-        //         timestamp: req.body.data[i][1],
-        //         source_name: req.body.data[i][2],
-        //         target_name: req.body.data[i][3],
-        //         inflictor_name: req.body.data[i][4],
-        //     })
-        // }
-        //
-        // else if (req.body.data[i][0] === 'DOTA_COMBATLOG_MODIFIER_REMOVE') {
-        //     ModifierRemove.create({
-        //         timestamp: req.body.data[i][1],
-        //         source_name: req.body.data[i][2],
-        //         target_name: req.body.data[i][3],
-        //         inflictor_name: req.body.data[i][4],
-        //     })
-        // }
-        //
-        // else if (req.body.data[i][0] === 'DOTA_COMBATLOG_DEATH') {
-        //     Death.create({
-        //         timestamp: req.body.data[i][1],
-        //         source_name: req.body.data[i][2],
-        //         target_name: req.body.data[i][3]
-        //     })
-        // }
-        //
-        // else if (req.body.data[i][0] === 'DOTA_COMBATLOG_ABILITY') {
-        //     Ability.create({
-        //         timestamp: req.body.data[i][1],
-        //         source_name: req.body.data[i][2],
-        //         target_name: req.body.data[i][3],
-        //         inflictor_name: req.body.data[i][4],
-        //         ability_level: req.body.data[i][5],
-        //         toggle_value: req.body.data[i][6]
-        //
-        //     })
-        // }
-        //
-        // else if (req.body.data[i][0] === 'DOTA_COMBATLOG_ITEM') {
-        //     Item.create({
-        //         timestamp: req.body.data[i][1],
-        //         source_name: req.body.data[i][2],
-        //         inflictor_name: req.body.data[i][3]
-        //     })
-        // }
-        //
-        // else if (req.body.data[i][0] === 'DOTA_COMBATLOG_GOLD') {
-        //     Gold.create({
-        //         timestamp: req.body.data[i][1],
-        //         source_name: req.body.data[i][2],
-        //         gold_amount: req.body.data[i][3]
-        //     })
-        // }
-        //
-        // else if (req.body.data[i][0] === 'DOTA_COMBATLOG_XP') {
-        //     Xp.create({
-        //         timestamp: req.body.data[i][1],
-        //         source_name: req.body.data[i][2],
-        //         xp_value: req.body.data[i][3]
-        //     })
-        // }
-        //
-        // else if (req.body.data[i][0] === 'DOTA_COMBATLOG_PURCHASE') {
-        //     Purchase.create({
-        //         timestamp: req.body.data[i][1],
-        //         source_name: req.body.data[i][2],
-        //         purchase: req.body.data[i][3]
-        //     })
-        // }
-        //
-        // else if (req.body.data[i][0] === 'DOTA_STATUS_UPDATE') {
-        //     Update.create({
-        //         timestamp: req.body.data[i][1],
-        //         source_name: req.body.data[i][2],
-        //         kills: req.body.data[i][3],
-        //         deaths: req.body.data[i][4],
-        //         assists: req.body.data[i][5],
-        //         last_hits: req.body.data[i][6],
-        //         total_gold: req.body.data[i][7],
-        //         total_xp: req.body.data[i][8],
-        //         teamfight_participation: req.body.data[i][9],
-        //         current_item_list: req.body.data[i][10],
-        //         life_state: req.body.data[i][11],
-        //         x: req.body.data[i][12],
-        //         y: req.body.data[i][13],
-        //         team_id: req.body.data[i][14]
-        //     })
-        // }
+        else if (req.body.data[i][0] === 'DOTA_COMBATLOG_HEAL') {
+            Heal.create({
+                timestamp: req.body.data[i][1],
+                source_name: req.body.data[i][2],
+                target_name: req.body.data[i][3],
+                inflictor_name: req.body.data[i][4],
+                heal_value: req.body.data[i][5],
+                health_remaining: req.body.data[i][6]
+            })
+        }
+
+        else if (req.body.data[i][0] === 'DOTA_COMBATLOG_MODIFIER_ADD') {
+            ModifierAdd.create({
+                timestamp: req.body.data[i][1],
+                source_name: req.body.data[i][2],
+                target_name: req.body.data[i][3],
+                inflictor_name: req.body.data[i][4],
+            })
+        }
+
+        else if (req.body.data[i][0] === 'DOTA_COMBATLOG_MODIFIER_REMOVE') {
+            ModifierRemove.create({
+                timestamp: req.body.data[i][1],
+                source_name: req.body.data[i][2],
+                target_name: req.body.data[i][3],
+                inflictor_name: req.body.data[i][4],
+            })
+        }
+
+        else if (req.body.data[i][0] === 'DOTA_COMBATLOG_DEATH') {
+            Death.create({
+                timestamp: req.body.data[i][1],
+                source_name: req.body.data[i][2],
+                target_name: req.body.data[i][3]
+            })
+        }
+
+        else if (req.body.data[i][0] === 'DOTA_COMBATLOG_ABILITY') {
+            Ability.create({
+                timestamp: req.body.data[i][1],
+                source_name: req.body.data[i][2],
+                target_name: req.body.data[i][3],
+                inflictor_name: req.body.data[i][4],
+                ability_level: req.body.data[i][5],
+                toggle_value: req.body.data[i][6]
+
+            })
+        }
+
+        else if (req.body.data[i][0] === 'DOTA_COMBATLOG_ITEM') {
+            Item.create({
+                timestamp: req.body.data[i][1],
+                source_name: req.body.data[i][2],
+                inflictor_name: req.body.data[i][3]
+            })
+        }
+
+        else if (req.body.data[i][0] === 'DOTA_COMBATLOG_GOLD') {
+            Gold.create({
+                timestamp: req.body.data[i][1],
+                source_name: req.body.data[i][2],
+                gold_amount: req.body.data[i][3]
+            })
+        }
+
+        else if (req.body.data[i][0] === 'DOTA_COMBATLOG_XP') {
+            Xp.create({
+                timestamp: req.body.data[i][1],
+                source_name: req.body.data[i][2],
+                xp_value: req.body.data[i][3]
+            })
+        }
+
+        else if (req.body.data[i][0] === 'DOTA_COMBATLOG_PURCHASE') {
+            Purchase.create({
+                timestamp: req.body.data[i][1],
+                source_name: req.body.data[i][2],
+                purchase: req.body.data[i][3]
+            })
+        }
+
+        else if (req.body.data[i][0] === 'DOTA_STATUS_UPDATE') {
+            Update.create({
+                timestamp: req.body.data[i][1],
+                source_name: req.body.data[i][2],
+                kills: req.body.data[i][3],
+                deaths: req.body.data[i][4],
+                assists: req.body.data[i][5],
+                last_hits: req.body.data[i][6],
+                total_gold: req.body.data[i][7],
+                total_xp: req.body.data[i][8],
+                teamfight_participation: req.body.data[i][9],
+                current_item_list: req.body.data[i][10],
+                life_state: req.body.data[i][11],
+                x: req.body.data[i][12],
+                y: req.body.data[i][13],
+                team_id: req.body.data[i][14]
+            })
+        }
 
 
     }
